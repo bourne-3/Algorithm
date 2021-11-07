56e1af95c1d9fab3193b542ada0f8525570ccbee
bourne-3 <595962708@qq.com>
2021-11-07 11:56:30 +0800

    测试文件

--- src/main/java/OutSide/test1/t01.java
+++ src/main/java/OutSide/test1/t01.java
@@ -1,46 +1,15 @@
 package OutSide.test1;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 /**
- * 子集问题
+
  */
 public class t01 {
-    List<List<Integer>> res = new ArrayList<>();
-    List<Integer> tmp = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        t01 t = new t01();
-        t.subSet(new int[]{1, 2, 3});
-
-
-
     }
 
+    public void T() {
 
-
-    public void subSet(int[] nums) {
-        backtracking(nums, 0);
-
-        for (List<Integer> i : res) {
-            System.out.print(i + " ");
-        }
     }
-
-    private void backtracking(int[] nums, int startIdx) {
-        res.add(new ArrayList<>(tmp));
-
-        for (int i = startIdx; i < nums.length; i++) {
-            tmp.add(nums[i]);
-            backtracking(nums, i + 1);
-            tmp.remove(tmp.size() - 1);
-        }
-    }
-
-
 }
-
-
