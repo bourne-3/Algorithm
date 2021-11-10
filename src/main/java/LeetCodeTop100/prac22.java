4bd95d3fe5975aa223d06a4f0c5ef80b49ab35c1
bourne-3 <595962708@qq.com>
2021-11-10 15:27:41 +0800

    test

--- src/main/java/LeetCodeTop100/prac22.java
+++ src/main/java/LeetCodeTop100/prac22.java
@@ -16,6 +16,20 @@ public class prac22 {
 
     }
 
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0){
+                    dp[i][j] = 1;
+                }else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, ((o1, o2) -> Integer.compare(o1[0], o2[0])));
         int start = intervals[0][0];
