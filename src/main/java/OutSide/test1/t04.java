bd890bb14e96eb48e87bb30fd8eabe64ec7b6fe8
bourne-3 <595962708@qq.com>
2021-11-15 19:46:12 +0800

    add some markdown file

--- src/main/java/OutSide/test1/t04.java
+++ src/main/java/OutSide/test1/t04.java
@@ -1,5 +1,8 @@
 package OutSide.test1;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
 
  */
@@ -8,8 +11,34 @@ public class t04 {
     public static void main(String[] args) {
 
     }
+    // 两个栈实现队列
+    Deque<Integer> in;
+    Deque<Integer> out;
 
-    public void T() {
+    public t04() {
+        in = new LinkedList<>();
+        out = new LinkedList<>();
+    }
 
+    public void push(int x) {
+        in.push(x);
+
+    }
+
+    public int pop() {
+        if (!out.isEmpty()) return out.pop();
+        if (in.isEmpty()) return -1;
+        while (!in.isEmpty()) {
+            out.push(in.pop());
+        }
+        return out.pop();
+    }
+
+    public int peek() {
+        return out.peek();
+    }
+
+    public boolean empty() {
+        return out.isEmpty();
     }
 }
