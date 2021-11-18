0b79681db9c044b45ff8a4e7a6a42eacf42ef992
bourne-3 <595962708@qq.com>
2021-11-18 23:14:23 +0800

    practice

--- src/main/java/CodeTop/Tencent/prac38_2.java
+++ src/main/java/CodeTop/Tencent/prac38_2.java
@@ -1,5 +1,7 @@
 package CodeTop.Tencent;
 
+import java.util.Arrays;
+
 /**
  给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
 
@@ -13,6 +15,20 @@ public class prac38_2 {
     }
 
     public int coinChange(int[] coins, int amount) {
+        // 背包：金额  物品：金币
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
 
+        // 里面是物品， 那么外面就是背包
+        for (int i = 0; i <= amount; i++) {
+            for (int coin:coins){
+                if (i - coin >= 0){
+                    dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+                }
+            }
+        }
+
+        return dp[amount] != amount + 1 ? dp[amount] : -1;
     }
 }
