d96751c780b5d2e5f9765e909ddabc3a0af9e05d
bourne-3 <595962708@qq.com>
2021-11-18 22:53:39 +0800

    practice

---
+++ src/main/java/CodeTop/Tencent/prac38_1.java
@@ -0,0 +1,22 @@
+package CodeTop.Tencent;
+
+/**
+ * 注意是组合，不是最小值
+ *
+ 给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+
+ 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+
+ 假设每一种面额的硬币有无限个。 
+
+ */
+public class prac38_1 {
+
+    public static void main(String[] args) {
+
+    }
+
+//    public int change(int amount, int[] coins) {
+//
+//    }
+}
---
+++ src/main/java/CodeTop/Tencent/prac38_2.java
@@ -0,0 +1,18 @@
+package CodeTop.Tencent;
+
+/**
+ 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+
+ 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+
+ */
+public class prac38_2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int coinChange(int[] coins, int amount) {
+
+    }
+}
